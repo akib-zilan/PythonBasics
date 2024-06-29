c659f2312dd9db1afba5ccbdc4bc675e85f8cbee
akib-zilan <akibzilan@gmail.com>
2024-06-29 16:25:18 +0600

    Created First few Codes

---
+++ Basic Calculator.py
@@ -0,0 +1,22 @@
+print("Select an Operation to Perform: ")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+operation = input()
+
+if operation == "1": 
+    # code for add
+    pass
+elif operation == "2":
+    # code for subtract
+    pass
+elif operation == "3":
+    # code for multiply
+    pass
+elif operation == "4":
+    # code for divide
+    pass
+else:
+    print("Invalid input")
