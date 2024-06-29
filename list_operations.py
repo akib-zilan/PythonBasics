a49e24de69a33aa25d5dfc73433f79317091a2b1
akib-zilan <akibzilan@gmail.com>
2024-06-29 19:12:04 +0600

    Data Type Int

--- list_operations.py
+++ list_operations.py
@@ -1,10 +1,10 @@
 #Create a program that takes a list of numbers and returns the maximum, minimum, and average values.
 
-n = input("Enter how many numbers you'll provide : ")
+n = int(input("Enter how many numbers you'll provide : "))
 a_list = list()
 for num in range(n) :
     num = input("Input the number : ")
     a_list.append(num)
 
-    
+
 print("Your Numbers are: ", a_list)
